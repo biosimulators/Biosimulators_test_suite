67cd978b38fafb290e4313dab00b71c637dc3ea4
Jonathan Karr <jonrkarr@gmail.com>
2020-12-30 00:05:51 -0500

    adding tests for Docker images

--- biosimulators_test_suite/test_case/docker_image.py
+++ biosimulators_test_suite/test_case/docker_image.py
@@ -8,9 +8,70 @@
 
 from ..data_model import TestCase
 from ..warnings import TestCaseWarning
+from biosimulators_utils.simulator.environ import ENVIRONMENT_VARIABLES
 import warnings
 
-__all__ = ['HasOciLabels', 'HasBioContainersLabels']
+__all__ = [
+    'DefaultUserIsRoot',
+    'DeclaresSupportedEnvironmentVariables',
+    'HasOciLabels',
+    'HasBioContainersLabels',
+]
+
+
+class DefaultUserIsRoot(TestCase):
+    """ Test that the default user of a Docker image is root """
+
+    def eval(self, specifications, expected_user=(None, '', '0')):
+        """ Evaluate a simulator's performance on a test case
+
+        Args:
+            specifications (:obj:`dict`): specifications of the simulator to validate
+            expected_user (:obj:`tuple`, optional): expected user
+
+        Raises:
+            :obj:`Exception`: if the simulator did not pass the test case
+        """
+        image = self.get_simulator_docker_image(specifications)
+        user = image.attrs['Config']['User']
+        if user not in expected_user:
+            msg = ("The default user for the Docker image is `{}`. For compatability with Singularity, "
+                   "Docker images for simulators should not declare default users (`USER`) other than root. "
+                   "Compatability with Singularity is important for using the image on HPC systems, including "
+                   "the infrastructure used by runBioSimulations and BioSimulations.\n\n"
+                   "More information:\n"
+                   "  https://biosimulators.org/standards/simulator-images\n"
+                   "  https://sylabs.io/guides/3.7/user-guide/singularity_and_docker.html#best-practices"
+                   ).format(user)
+            warnings.warn(msg, TestCaseWarning)
+
+
+class DeclaresSupportedEnvironmentVariables(TestCase):
+    """ Test if a Docker image declares the environment variables that is supports """
+
+    def eval(self, specifications):
+        """ Evaluate a simulator's performance on a test case
+
+        Args:
+            specifications (:obj:`dict`): specifications of the simulator to validate
+
+        Raises:
+            :obj:`Exception`: if the simulator did not pass the test case
+        """
+        image = self.get_simulator_docker_image(specifications)
+        expected_env_vars = set(var.name for var in ENVIRONMENT_VARIABLES.values())
+        env_vars = set(key_val.partition('=')[0] for key_val in image.attrs['Config']['Env'])
+        potentially_missing_env_vars = sorted(expected_env_vars.difference(env_vars))
+        if potentially_missing_env_vars:
+            msg = ('Docker images for simulation tools should declare the environment variables that they support.\n\n'
+                   'The Docker image does not declare the following standard environment '
+                   'variables recognized by BioSimulators:\n  - {}\n\n'
+                   'If the simulation tool implements these variables, they should be declared in the Dockerfile for '
+                   'the Docker image for the simulator.\n\n'
+                   'Note, support for these environment variables is optional. Simulation tools are not requireed to support '
+                   'these variables.'
+                   ).format('\n  - '.join("'" + var + "'" for var in potentially_missing_env_vars))
+            warnings.warn(msg, TestCaseWarning)
 
 
 class HasOciLabels(TestCase):
--- tests/test_case/test_docker_image.py
+++ tests/test_case/test_docker_image.py
@@ -1,18 +1,29 @@
 from biosimulators_test_suite.test_case import docker_image
+from biosimulators_test_suite.warnings import TestCaseWarning
 import unittest
 
 
 class DockerImageTestCaseTest(unittest.TestCase):
     IMAGE = 'ghcr.io/biosimulators/biosimulators_copasi/copasi:latest'
 
+    def test_DefaultUserIsRoot(self):
+        case = docker_image.DefaultUserIsRoot()
+        with self.assertWarnsRegex(TestCaseWarning, 'should not declare default users'):
+            case.eval({'image': {'url': 'hello-world'}}, expected_user=('undefined',))
+
+    def test_DeclaresSupportedEnvironmentVariables(self):
+        case = docker_image.DeclaresSupportedEnvironmentVariables()
+        with self.assertWarnsRegex(TestCaseWarning, 'should declare the environment variables'):
+            case.eval({'image': {'url': 'hello-world'}})
+
     def test_HasOciLabels(self):
         case = docker_image.HasOciLabels()
         case.eval({'image': {'url': self.IMAGE}})
-        with self.assertWarnsRegex(UserWarning, 'should have the following'):
+        with self.assertWarnsRegex(TestCaseWarning, 'should have the following'):
             case.eval({'image': {'url': 'hello-world'}})
 
     def test_HasBioContainersLabels(self):
         case = docker_image.HasBioContainersLabels()
         case.eval({'image': {'url': self.IMAGE}})
-        with self.assertWarnsRegex(UserWarning, 'should have the following'):
+        with self.assertWarnsRegex(TestCaseWarning, 'should have the following'):
             case.eval({'image': {'url': 'hello-world'}})
